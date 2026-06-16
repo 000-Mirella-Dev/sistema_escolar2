d34d315ab62af201d17e80c52c2b7a0cb39b1c76
abel maia sousa <abel.sousa217@gmail.com>
2026-06-16 04:16:21 -0300

    implementação componente busca

--- my-app/app/components/busca.jsx
+++ my-app/app/components/busca.jsx
@@ -1,49 +1,48 @@
 
-
 import { useState } from "react";
 
-export default function Buscar() {
+export default function Buscar({ onResultadoEncontrado, onCarregando }) {
   const [termo, setTermo] = useState("");
-  const [usuarios, setUsuarios] = useState([]);
 
-  async function buscar() {
+  async function buscar(e) {
+    e.preventDefault()
     if (!termo.trim()) {
-      setUsuarios([]);
+      onResultadoEncontrado([]);
       return;
     }
-
-    const res = await fetch(`/api/users?busca=${encodeURIComponent(termo)}`);
-    const data = await res.json();
-
-    setUsuarios(data);
+    onCarregando(true)
+    try{
+      const res = await fetch(`/api/buscarUsers?busca=${encodeURIComponent(termo)}`);
+      const data = await res.json();
+      onResultadoEncontrado(data)
+    } catch (erro){
+      console.error(erro)
+      alert("erro ao buscar")
+    } finally {
+      onCarregando(false)
+    }
   }
 
   return (
     <div style={{ padding: 20 }}>
       <h1 className="text-3xl font-extrabold bg-linear-to-r from-blue-900 bg-clip-text text-transparent p-4">Pesquisar usuários</h1>
-     <input
-        value={termo}
-        onChange={(e) => setTermo(e.target.value)}
-        placeholder="Insira um nome ou função"
-        className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:border-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
+      <form onSubmit={buscar}>
+        <input
+            value={termo}
+            onChange={(e) => setTermo(e.target.value)}
+            placeholder="Insira um nome ou função"
+            className="w-full rounded-lg border border-gray-300 px-4 py-2 focus:border-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
 
-      <button
-        className="px-3 py-2 bg-blue-100 hover:bg-blue-200 text-blue-700 rounded-lg transition-colors duration-200 m-4"
-        onClick={buscar}
-        style={{ marginLeft: 10 }}
-      >
-        Pesquisar
-      </button>
-
-      <div className="mt-4">
-        {usuarios.map((u, index) => (
-          <div key={index} className="border rounded-lg p-3 mb-3 shadow-sm max-w-full">
-            <strong>{u.nome}</strong>
-            <p className="text-gray-500">{u.perfil}</p>
-          </div>
-        ))}
-      </div>
+          <button
+            type="submit"
+            className="px-3 py-2 bg-blue-100 hover:bg-blue-200 text-blue-700 rounded-lg transition-colors duration-200 m-4"
+            onClick={buscar}
+            style={{ marginLeft: 10 }}
+          >
+            Pesquisar
+          </button>
+      </form>
     </div>
   );
 }
