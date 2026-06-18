47badbda0428fdaaf228792161789c67b028fa04
Mirella-Dev <m.500.who@gmail.com>
2026-06-18 00:53:49 -0300

    versão final

--- my-app/app/api/excluirUsuarios/route.js
+++ my-app/app/api/excluirUsuarios/route.js
@@ -1,19 +1,88 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const result = await pool.query(
-  `
-  SELECT 
-    n.id,
-    n.disciplina,
-    n.nota1,
-    n.nota2,
-    n.nota3,
-    n.nota4,
-    u.nome AS aluno_nome
-  FROM notas n
-  JOIN usuarios u ON u.id = n.aluno_id
-  WHERE n.professor_id = $1
-  ORDER BY n.id DESC
-  LIMIT 200
-  `,
-  [professorId]
-);
+
+
+import pool from "@/lib/db";
+import { cookies } from "next/headers";
+
+export async function DELETE(req) {
+  try {
+
+    const { email } = await req.json();
+
+    const cookieStore = await cookies();
+
+    const admin = JSON.parse(
+      decodeURIComponent(
+        cookieStore.get("usuario").value
+      )
+    );
+
+    const usuario = await pool.query(
+      `
+      SELECT *
+      FROM usuarios
+      WHERE email = $1
+      `,
+      [email]
+    );
+
+    if (usuario.rowCount === 0) {
+      return Response.json(
+        { erro: "Email não encontrado" },
+        { status: 404 }
+      );
+    }
+
+    const usuarioExcluido = usuario.rows[0];
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const result = await pool.query(
+      `
+      DELETE FROM usuarios
+      WHERE email = $1
+      RETURNING *
+      `,
+      [email]
+    );
+
+    try {
+      await pool.query(
+        `
+        INSERT INTO auditoria
+        (
+          usuario_id,
+          tabela_afetada,
+          registro_id,
+          tipo_alteracao,
+          descricao
+        )
+        VALUES
+        ($1,$2,$3,$4,$5)
+        `,
+        [
+          admin.id,
+          "usuarios",
+          usuarioExcluido.id,
+          "DELETE",
+          `${admin.nome} excluiu o usuário ${usuarioExcluido.nome} (${usuarioExcluido.email})`
+        ]
+      );
+    } catch (erroAuditoria) {
+      console.error(
+        "Erro ao registrar auditoria:",
+        erroAuditoria
+      );
+    }
+
+    return Response.json({
+      mensagem: `Usuário ${email} excluído com sucesso`,
+    });
+
+  } catch (erro) {
+
+    return Response.json(
+      { erro: erro.message },
+      { status: 500 }
+    );
+
+  }
+}
